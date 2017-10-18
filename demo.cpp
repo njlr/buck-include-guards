f413565e632dfa154251caf4bea0e9d039de57fd
njlr <njlr@users.noreply.github.com>
2017-10-18 14:44:57 +0100

    * Added another function

--- demo.cpp
+++ demo.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
 #include <mathutils/add.hpp>
+#include <mathutils/add5.hpp>
 #include <mathutils/sub.hpp>
 
 int main() {
   std::cout << "add(1, 2) = " << add(1, 2) << std::endl;
   std::cout << "sub(4, 3) = " << sub(4, 3) << std::endl;
+  std::cout << "add5(40) = " << add5(40) << std::endl;
   return 0;
 }
---
+++ mathutils/include/add5.hpp
@@ -0,0 +1,5 @@
+#include <mathutils/add.hpp>
+
+int add5(int x) {
+  return add(x, 5);
+}
